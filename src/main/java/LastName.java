814710e811a10ced0e5fdf3fba194aed5a5a31e2
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-02 16:01:56 +0530

    [Prajwal]:UC1:Valid Last Name

---
+++ src/main/java/LastName.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class LastName {
+
+    public static void main(String[] args) {
+        Predicate<String> firstLetter=(name)->Character.isUpperCase(name.charAt(0));
+        Predicate<String> namelength=(name)->name.length()>=3;
+        Predicate<String> validLastName=firstLetter.and(namelength);
+
+        Scanner sc=new Scanner(System.in);
+        String lastName=sc.nextLine();
+
+        if(validLastName.test(lastName)){
+            System.out.println("Name is valid");
+        }else{
+            System.out.println("Name is not valid");
+        }
+    }
+
+}
